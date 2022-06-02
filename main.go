53c621aacf202effaa0fc13c809faf0b6828964f
Ajil Raju <ajilraju.m@gmail.com>
2022-06-02 08:12:24 +0530

    Changed banner

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	var f_op int
 	var s_op int
 
-	fmt.Println("Golang Demo calculator")
+	fmt.Println("Golang Demo calc")
 	fmt.Println("======================")
 
 	fmt.Println("Enter the first digit")
