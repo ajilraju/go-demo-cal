feacec04f5e45cbf5924b7988a6a77d0ddf0136c
Ajil Raju <ajilraju.m@gmail.com>
2022-08-21 23:36:33 +0530

    added cli ascii color

--- main.go
+++ main.go
@@ -3,6 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+)
+
+var (
+	BOLD   string = "\033[1m"
+	YELLOW string = "\033[93m"
+	BLUE   string = "\033[94m"
+	END    string = "\033[0m"
 )
 
 func add(f int, s int) int {
@@ -15,24 +23,23 @@ func multi(f int, s int) int {
 
 func main() {
 
-	var f_op int
-	var s_op int
+	var firstNum int
+	var secondNum int
 
-	fmt.Println("Golang Demo Calc")
-	fmt.Println("======================")
+	fmt.Printf("%sAncient simple Calculator%s\n\n", BOLD, END)
 
-	fmt.Println("Enter the first digit")
-	if _, err := fmt.Scan(&f_op); err != nil {
+	fmt.Println("Enter the first digit: ")
+	if _, err := fmt.Scan(&firstNum); err != nil {
 		log.Print("Failed to scan first number", err)
 		return
 	}
 
 	fmt.Println("Enter the second digit")
-	if _, err := fmt.Scan(&s_op); err != nil {
+	if _, err := fmt.Scan(&secondNum); err != nil {
 		log.Print("Failed to scan second number", err)
 		return
 	}
 
-	fmt.Printf("Sum of %d and %d is %d\n", f_op, s_op, add(f_op, s_op))
-	fmt.Printf("Product of %d and %d is %d\n", f_op, s_op, multi(f_op, s_op))
+	fmt.Printf("%sSum     [ %d + %d = %d ]%s\n", BLUE, firstNum, secondNum, add(firstNum, secondNum), END)
+	fmt.Printf("%sProduct [ %d * %d = %d ]%s\n", YELLOW, firstNum, secondNum, multi(firstNum, secondNum), END)
 }
