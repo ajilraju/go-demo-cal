4084164be3c4677c1301785a8317ceeebbf808e2
Ajil Raju <ajilraju.m@gmail.com>
2022-08-21 23:47:34 +0530

    changed tab to spaces

--- main.go
+++ main.go
@@ -1,56 +1,56 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
+    "fmt"
+    "log"
+    "os"
 )
 
 var APP_VERSION string = "1.0.0"
 
 var (
-	BOLD   string = "\033[1m"
-	YELLOW string = "\033[93m"
-	BLUE   string = "\033[94m"
-	END    string = "\033[0m"
+    BOLD   string = "\033[1m"
+    YELLOW string = "\033[93m"
+    BLUE   string = "\033[94m"
+    END    string = "\033[0m"
 )
 
 func add(f int, s int) int {
-	return f + s
+    return f + s
 }
 
 func multi(f int, s int) int {
-	return f * s
+    return f * s
 }
 
 func main() {
 
-	var firstNum int
-	var secondNum int
+    var firstNum int
+    var secondNum int
 
-	if len(os.Args) == 2 {
-		if os.Args[1] == "-v" {
-			fmt.Printf("%sAncient simple Calculator: %s%s\n", BOLD, APP_VERSION, END)
-			return
-		} else {
-			return
-		}
-	}
+    if len(os.Args) == 2 {
+        if os.Args[1] == "-v" {
+            fmt.Printf("%sAncient simple Calculator: %s%s\n", BOLD, APP_VERSION, END)
+            return
+        } else {
+            return
+        }
+    }
 
-	fmt.Printf("%sAncient simple Calculator%s\n\n", BOLD, END)
+    fmt.Printf("%sAncient simple Calculator%s\n\n", BOLD, END)
 
-	fmt.Println("Enter the first digit: ")
-	if _, err := fmt.Scan(&firstNum); err != nil {
-		log.Print("Failed to scan first number", err)
-		return
-	}
+    fmt.Println("Enter the first digit: ")
+    if _, err := fmt.Scan(&firstNum); err != nil {
+        log.Print("Failed to scan first number", err)
+        return
+    }
 
-	fmt.Println("Enter the second digit")
-	if _, err := fmt.Scan(&secondNum); err != nil {
-		log.Print("Failed to scan second number", err)
-		return
-	}
+    fmt.Println("Enter the second digit")
+    if _, err := fmt.Scan(&secondNum); err != nil {
+        log.Print("Failed to scan second number", err)
+        return
+    }
 
-	fmt.Printf("%sSum     [ %d + %d = %d ]%s\n", BLUE, firstNum, secondNum, add(firstNum, secondNum), END)
-	fmt.Printf("%sProduct [ %d * %d = %d ]%s\n", YELLOW, firstNum, secondNum, multi(firstNum, secondNum), END)
+    fmt.Printf("%sSum     [ %d + %d = %d ]%s\n", BLUE, firstNum, secondNum, add(firstNum, secondNum), END)
+    fmt.Printf("%sProduct [ %d * %d = %d ]%s\n", YELLOW, firstNum, secondNum, multi(firstNum, secondNum), END)
 }
